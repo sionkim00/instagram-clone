be5dbd8f17a303d27a509aaf4ffc94051bd78e45
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2022-04-09 17:40:30 +0900

    Bottom tab navigation

--- App.js
+++ App.js
@@ -1,20 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
+import React from "react";
+import Navigation from "./Navigation";
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Instagram</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
